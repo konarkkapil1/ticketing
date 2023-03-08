49d1f9e410cb4723cff44f2ed1c0512a4b115b0f
konarkkapil1 <konarkkapil@rocketmail.com>
2023-03-08 21:51:39 +0530

    sample run

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payement-created-listener";
 
 const start = async () => {
+  console.log(`individual test`);
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
